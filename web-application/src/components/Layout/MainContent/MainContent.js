03b16db0c498da2da824df554ab367d3db84fabc
gianinachis13 <61515211+gianinachis13@users.noreply.github.com>
2023-01-14 23:35:30 +0200

    Display flex cards and mobile header

--- web-application/src/components/Layout/MainContent/MainContent.js
+++ web-application/src/components/Layout/MainContent/MainContent.js
@@ -1,21 +1,147 @@
 import React from 'react'
 import './MainContent.scss'
 import Login from '../../pages/Login/Login'
-
+import img from '../../images/GOAT.png'
 function Header() {
 
     return (
-            <div className="grid-main-container">
-                <div className="main-item">1</div>
-                <div className="main-item">2</div>
-                <div className="main-item">3</div>
-                <div className="main-item">4</div>
-                <div className="main-item">5</div>
-                <div className="main-item">6</div>
-                <div className="main-item">7</div>
-                <div className="main-item">8</div>
-                <div className="main-item">9</div>
+
+
+        <div className='main-content'>
+            <div className='cards'>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <h3>Title</h3>
+                            <img src={img} />
+                        </div>
+                        <div class="flip-card-back">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled </p>
+                            <p>Price: 10 RON/mp</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <h3>Title</h3>
+                            <img src={img} />
+                        </div>
+                        <div class="flip-card-back">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled </p>
+                            <p>Price: 10 RON/mp</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <h3>Title</h3>
+                            <img src={img} />
+                        </div>
+                        <div class="flip-card-back">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled </p>
+                            <p>Price: 10 RON/mp</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <h3>Title</h3>
+                            <img src={img} />
+                        </div>
+                        <div class="flip-card-back">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled </p>
+                            <p>Price: 10 RON/mp</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <h3>Title</h3>
+                            <img src={img} />
+                        </div>
+                        <div class="flip-card-back">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled </p>
+                            <p>Price: 10 RON/mp</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <h3>Title</h3>
+                            <img src={img} />
+                        </div>
+                        <div class="flip-card-back">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled </p>
+                            <p>Price: 10 RON/mp</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <h3>Title</h3>
+                            <img src={img} />
+                        </div>
+                        <div class="flip-card-back">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled </p>
+                            <p>Price: 10 RON/mp</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <h3>Title</h3>
+                            <img src={img} />
+                        </div>
+                        <div class="flip-card-back">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled </p>
+                            <p>Price: 10 RON/mp</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <h3>Title</h3>
+                            <img src={img} />
+                        </div>
+                        <div class="flip-card-back">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled </p>
+                            <p>Price: 10 RON/mp</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <h3>Title</h3>
+                            <img src={img} />
+                        </div>
+                        <div class="flip-card-back">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled </p>
+                            <p>Price: 10 RON/mp</p>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
+
     )
 }
 
